a5ddac67866087398e1205e0590cb3641b71aec3
MTJV <64918556+MTJV@users.noreply.github.com>
2025-09-26 01:23:35 +0300

    SP500.py

--- SP500.py
+++ SP500.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-st.autorefresh(interval=1 * 60 * 60 * 1000) #refreshes data every hour 
+st.experimental_autorefresh(interval=1 * 60 * 60 * 1000) #refreshes data every hour 
 
 try:
     import streamlit as st
